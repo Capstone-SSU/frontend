212da96ae838755da4a25e8c284558961cb5d1cf
안채완 <59831262+eekrwl@users.noreply.github.com>
2022-02-26 12:33:31 +0900

    [추가] 로그인 아주조금

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,9 +1,44 @@
 import React from "react";
 import '../pages_css/Signin.css';
+import logo from '../logo.png';
+import githubLogo from '../githubLogo.png';
 
 const Signin = () => {
   return (
-    <div></div>
+    <div className="App">
+      <div className="inner">
+        <img src={logo} id="mainLogo"></img>
+        <LoginForm></LoginForm>
+        <div className="sub_menu">
+          <a>이메일 가입</a>
+          <a>비밀번호 찾기</a>
+        </div>
+        <GithubLogin></GithubLogin>
+      </div>
+
+    </div>
+  );
+}
+
+const LoginForm = () => {
+  return (
+    <form>
+      <p className="input_label">이메일 주소</p>
+      <input type="text" className="input_form"></input>
+      <p className="input_label">비밀번호</p>
+      <input type="password" className="input_form"></input>
+      <input type="submit" id="submit_button"></input>
+    </form>
+  );
+}
+
+const GithubLogin = () => {
+  return (
+    <div className="github_login_button">
+      <img src={githubLogo} id="githubLogo"></img>
+
+      <p>깃허브로 로그인</p>
+    </div>
   );
 }
 
