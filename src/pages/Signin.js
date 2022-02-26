5c2517d8e196d63003805edbe094735df3ade967
choiyeju <yeju1019@gmail.com>
2022-02-26 17:56:11 +0900

    yeju
    make signin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -2,20 +2,20 @@ import React from "react";
 import '../pages_css/Signin.css';
 import logo from '../logo.png';
 import githubLogo from '../githubLogo.png';
+// import axios from 'axios';
 
 const Signin = () => {
   return (
-    <div className="App">
-      <div className="inner">
-        <img src={logo} id="mainLogo"></img>
+    
+    <div style={{ width:'100%', height: '900px', display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center', }}>
+      <div style={{}}>
+        <img src={logo} id="main_logo"></img>
         <LoginForm></LoginForm>
-        <div className="sub_menu">
-          <a>이메일 가입</a>
-          <a>비밀번호 찾기</a>
-        </div>
+
+        <div>이메일 가입&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;비밀번호 찾기</div>
+
         <GithubLogin></GithubLogin>
       </div>
-
     </div>
   );
 }
@@ -23,21 +23,26 @@ const Signin = () => {
 const LoginForm = () => {
   return (
     <form>
-      <p className="input_label">이메일 주소</p>
-      <input type="text" className="input_form"></input>
-      <p className="input_label">비밀번호</p>
-      <input type="password" className="input_form"></input>
-      <input type="submit" id="submit_button"></input>
+      <div className='main_emailPassword'>
+        <div id="main_email">이메일 주소</div> {/*input_label*/}
+        <input type='text' placeholder='abc123@abc123.com'/> {/*input_form*/}
+      </div>
+      <div className='main_emailPassword'>
+        <div id="main_password">비밀번호</div> {/*input_label*/}
+        <input type='text'/> {/*input_form*/}
+      </div>
+      <input type="submit" id="main_login" className="button"></input>
     </form>
   );
 }
 
 const GithubLogin = () => {
   return (
-    <div className="github_login_button">
-      <img src={githubLogo} id="githubLogo"></img>
-
-      <p>깃허브로 로그인</p>
+    <div>
+      <div style={{ width: '350px', height: '50px', margin: '30px 0px 0px 0px', display: 'flex', alignItems: 'center', border: '1px solid rgb(219, 219, 219)', }}>
+        <div style={{ width: '50px', }}><img src={githubLogo} id="main_githubLogo"/></div>
+        <div style={{ width: '265px', fontWeight: 'bolder', }}>깃허브로 로그인</div>
+      </div>
     </div>
   );
 }
